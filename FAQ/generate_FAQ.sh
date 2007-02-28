48dbe3bccd6f0f1106e8da2eec63e6705db46af3
Jordi Inglada <jordi.inglada@orfeo-toolbox.org>
2007-02-28 13:02:06 +0000

    mise a jour FAQ

--- FAQ/generate_FAQ.sh
+++ FAQ/generate_FAQ.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
+TEXINPUTS=$TEXINPUTS:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide/../Latex:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide/Latex:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide/Art:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide/Examples:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide/Art:/usr/local/stok/OTB/trunk/OTB-Documents/SoftwareGuide/Latex
+export TEXINPUTS
+
 latex2rtf OTB-FAQ.tex
 latex2html -split 0 -local_icons -info "" OTB-FAQ.tex
+perl -pi -e 's/http\:\/\/smsc.cnes.fr\/PLEIADES\/OTB\/Doxygen\/html\/classotb_1_1StreamingImageFileWriter.htmlotb\:\:StreamingImageFileWriter/\<a href=\"http\:\/\/smsc.cnes.fr\/PLEIADES\/OTB\/Doxygen\/html\/classotb_1_1StreamingImageFileWriter.html\"\>otb\:\:StreamingImageFileWriter\<\/a\>/g' OTB-FAQ/OTB-FAQ.html
+perl -pi -e 's/http\:\/\/smsc.cnes.fr\/PLEIADES\/OTB\/Doxygen\/html\/classitk_1_1StreamingImageFilter.htmlitk\:\:StreamingImageFilter/\<a href=\"http\:\/\/smsc.cnes.fr\/PLEIADES\/OTB\/Doxygen\/html\/classitk_1_1StreamingImageFilter.html\"\>itk\:\:StreamingImageFilter\<\/a\>/g' OTB-FAQ/OTB-FAQ.html
 pdflatex OTB-FAQ.tex
