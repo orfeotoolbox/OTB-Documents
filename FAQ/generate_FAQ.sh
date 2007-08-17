538a34fb56ed6ebba3d65dff2c14c2fe2ba960a6
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2007-08-17 08:36:06 +0000

    Ajout automatique du lien pour le tracker analytics (TODO confirmer le numero)

--- FAQ/generate_FAQ.sh
+++ FAQ/generate_FAQ.sh
@@ -7,4 +7,5 @@ latex2rtf OTB-FAQ.tex
 latex2html -split 0 -local_icons -info "" OTB-FAQ.tex
 perl -pi -e 's/http\:\/\/www.melaneum.com\/OTB\/doxygen\/classotb_1_1StreamingImageFileWriter.htmlotb\:\:StreamingImageFileWriter/\<a href=\"http\:\/\/www.melaneum.com\/OTB\/doxygen\/classotb_1_1StreamingImageFileWriter.html\"\>otb\:\:StreamingImageFileWriter\<\/a\>/g' OTB-FAQ/OTB-FAQ.html
 perl -pi -e 's/http\:\/\/www.melaneum.com\/OTB\/doxygen\/classitk_1_1StreamingImageFilter.htmlitk\:\:StreamingImageFilter/\<a href=\"http\:\/\/www.melaneum.com\/OTB\/doxygen\/classitk_1_1StreamingImageFilter.html\"\>itk\:\:StreamingImageFilter\<\/a\>/g' OTB-FAQ/OTB-FAQ.html
+perl -pi -e 's/\<\/BODY\>/\<script src="http:\/\/www.google-analytics.com\/urchin.js" type="text\/javascript"\> \<\/script\> \<script type="text\/javascript"\> _uacct = "UA-1482698-1"; urchinTracker(); \<\/script\> \<\/BODY\>/g' OTB-FAQ/OTB-FAQ.html
 pdflatex OTB-FAQ.tex
