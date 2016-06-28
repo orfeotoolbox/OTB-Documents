5fdaf3cc5aa08354ab59a8a092122def64e1a6f4
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2016-06-28 14:10:58 +0200

    DOC: fix pdf generation script

--- Courses/2016/WorkshopGuide/export_pdf.sh
+++ Courses/2016/WorkshopGuide/export_pdf.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-for f in workshop*.org
+for f in workshopSlides-*.org
 do
  filename=$(basename "$f")
  extension="${filename##*.}"
  filename="${filename%.*}"
  echo "Processing $f"
  emacs $filename.org --batch -f org-beamer-export-to-latex --kill
+ #add pdf version
+ sed -i '1 i \\\pdfminorversion=4' $filename.tex
+ #export to pdf
+ pdflatex -interaction nonstopmode $filename.tex
+ pdflatex -interaction nonstopmode $filename.tex
+done
+
+for f in workshopGuide-*.org
+do
+ filename=$(basename "$f")
+ extension="${filename##*.}"
+ filename="${filename%.*}"
+ echo "Processing $f"
+ emacs $filename.org --batch -f org-latex-export-to-latex --kill
  #add pdf version
  sed -i '1 i \\\pdfminorversion=4' $filename.tex
  #export to pdf
