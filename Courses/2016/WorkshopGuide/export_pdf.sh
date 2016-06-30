c6f3d9160079d8864bc44b62c84af34d93a26c34
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2016-06-30 13:49:22 +0200

    ENH: export solutions in bash script

--- Courses/2016/WorkshopGuide/export_pdf.sh
+++ Courses/2016/WorkshopGuide/export_pdf.sh
@@ -27,3 +27,17 @@ do
  pdflatex -interaction nonstopmode $filename.tex
  pdflatex -interaction nonstopmode $filename.tex
 done
+
+for f in workshopSolution-*.org
+do
+ filename=$(basename "$f")
+ extension="${filename##*.}"
+ filename="${filename%.*}"
+ echo "Processing $f"
+ emacs $filename.org --batch -f org-latex-export-to-latex --kill
+ #add pdf version
+ sed -i '1 i \\\pdfminorversion=4' $filename.tex
+ #export to pdf
+ pdflatex -interaction nonstopmode $filename.tex
+ pdflatex -interaction nonstopmode $filename.tex
+done
