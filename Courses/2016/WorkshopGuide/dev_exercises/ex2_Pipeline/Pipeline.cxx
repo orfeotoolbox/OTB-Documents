9f43c542e7a84bb6e5c2849bab2d4ade36077c7b
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2017-04-19 16:26:52 +0200

    ENH: add exercice to compute image gradient

--- Courses/2016/WorkshopGuide/dev_exercises/ex2_Pipeline/Pipeline.cxx
+++ Courses/2016/WorkshopGuide/dev_exercises/ex2_Pipeline/Pipeline.cxx
@@ -2,87 +2,37 @@
 #include "otbImageFileReader.h"
 #include "otbImageFileWriter.h"
 
+#include "itkGradientMagnitudeImageFilter.h" 
+
 int main(int argc, char * argv[])
 {
   if (argc != 3)
     {
     std::cerr << "Usage: "
-        << argv[0]
-        << " <input_filename> <output_filename>"
-        << std::endl;
+              << argv[0]
+              << " <input_filename> <output_filename>"
+              << std::endl;
     }
-// Software Guide : EndCodeSnippet
 
-  //  Software Guide : BeginLatex
-  //
-  //  Declare the image as an \doxygen{otb}{Image}, the pixel type
-  // is declared as an unsigned char (one byte) and the image is specified as
-  // having two dimensions.
-  //
-  //  Software Guide : EndLatex
-
-  // Software Guide : BeginCodeSnippet
-  typedef otb::Image<unsigned char, 2> ImageType;
-  // Software Guide : EndCodeSnippet
-
-  //  Software Guide : BeginLatex
-  //
-  //  To read the image, we need an \doxygen{otb}{ImageFileReader}
-  // which is templated with the image type.
-  //
-  //  Software Guide : EndLatex
-
-  // Software Guide : BeginCodeSnippet
+  typedef otb::Image<unsigned int, 2> ImageType;
   typedef otb::ImageFileReader<ImageType> ReaderType;
   ReaderType::Pointer reader = ReaderType::New();
-  // Software Guide : EndCodeSnippet
-
-  //  Software Guide : BeginLatex
-  //
-  // Then, we need an \doxygen{otb}{ImageFileWriter}
-  // also templated with the image type.
-  //
-  //  Software Guide : EndLatex
-
-  // Software Guide : BeginCodeSnippet
+  
   typedef otb::ImageFileWriter<ImageType> WriterType;
   WriterType::Pointer writer = WriterType::New();
-  // Software Guide : EndCodeSnippet
-
-  //  Software Guide : BeginLatex
-  //
-  // The filenames are passed as arguments to the program. We keep it
-  // simple for now and we don't check their validity.
-  //
-  //  Software Guide : EndLatex
-
-  // Software Guide : BeginCodeSnippet
+  
   reader->SetFileName(argv[1]);
   writer->SetFileName(argv[2]);
-  // Software Guide : EndCodeSnippet
 
-  //  Software Guide : BeginLatex
-  //
-  // Now that we have all the elements, we connect the pipeline,
-  // pluging the output of the reader to the input of the writer.
-  //
-  //  Software Guide : EndLatex
+  
+  typedef itk::GradientMagnitudeImageFilter <ImageType, ImageType> FilterType; 
 
-  // Software Guide : BeginCodeSnippet
-  writer->SetInput(reader->GetOutput());
-  // Software Guide : EndCodeSnippet
+  FilterType::Pointer filter = FilterType::New();
 
-  //  Software Guide : BeginLatex
-  //
-  // And finally, we trigger the pipeline execution calling the Update()
-  // method on the last element of the pipeline. The last element will make
-  // sure to update all previous elements in the pipeline.
-  //
-  //  Software Guide : EndLatex
-
-  // Software Guide : BeginCodeSnippet
+  filter->SetInput(reader->GetOutput()); 
+  writer->SetInput(filter->GetOutput());
+  
   writer->Update();
 
   return EXIT_SUCCESS;
 }
-// Software Guide : EndCodeSnippet
